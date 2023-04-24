492f9e98ada1ce3aa0ef7b97dc0313dd622d008a
Rik Harink <Rik.Harink@topicus.nl>
2023-04-24 17:09:54 +0200

    ydotool typing script

---
+++ type.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+case $1 in
+ 1)
+ ydotool type -f './voorbeelden/examples/count_1.rs'
+ ;;
+ 2)
+ ydotool type -f './voorbeelden/examples/count_2.rs'
+ ;;
+ 3)
+ ydotool type -f './voorbeelden/examples/count_3.rs'
+ ;;
+ 4)
+ ydotool type -f './voorbeelden/examples/enum_1.rs'
+ ;;
+ 5)
+ ydotool type -f './voorbeelden/examples/thread_1.rs'
+ ;;
+ 6)
+ ydotool type -f './voorbeelden/examples/thread_2.rs'
+ ;;
+ 7)
+ ydotool type -f './voorbeelden/examples/thread_3.rs'
+ ;;
+ 8)
+ ydotool type -f './voorbeelden/examples/thread_4.rs'
+ ;;
+ 9)
+ ydotool type -f './voorbeelden/examples/thread_5.rs'
+ ;;
+ 10)
+ ydotool type -f './voorbeelden/examples/thread_6.rs'
+ ;;
+ 11)
+ ydotool type -f './voorbeelden/examples/struct_1.rs'
+ ;;
+ 12)
+ ydotool type -f './voorbeelden/examples/struct_1.rs'
+ ;;
+esac
