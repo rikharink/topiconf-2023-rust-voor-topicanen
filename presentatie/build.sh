943e6e169dd92b6818d9b86fae124153af311cd0
Rik Harink <Rik.Harink@topicus.nl>
2023-04-26 10:47:25 +0200

    styling tweaks

--- presentatie/build.sh
+++ presentatie/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+rm -rf dist
 mkdir -p dist
 cp -r img dist
-cp -r themes dist
+cp -r themes/font/ dist
 npx @marp-team/marp-cli@latest -o "dist/index.html" --theme "./themes/topicus.css" ./index.md
 npx @marp-team/marp-cli@latest -o "dist/rust-voor-topicanen.pdf" --allow-local-files --pdf  --theme "./themes/topicus.css" ./index.md
 npx @marp-team/marp-cli@latest -o "dist/rust-voor-topicanen.pptx" --allow-local-files --pptx --theme "./themes/topicus.css" ./index.md
